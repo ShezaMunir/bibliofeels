21340f86d0f4a9adc363bfebd467c1853691f806
Omar <omarmuhammed1998.om@gmail.com>
2021-06-10 18:36:38 +0200

    create basic layout for the first page

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import Landing from '../views/Landing'
 
 export default class App extends Component {
   render() {
-    return <div>hello!</div>;
+    return (
+      <div>
+        <Landing/>
+      </div>
+    );
   }
 }

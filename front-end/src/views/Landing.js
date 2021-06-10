ef53420aabf1e0a4a1e5dbe6becec03819f8b0aa
Omar <omarmuhammed1998.om@gmail.com>
2021-06-11 01:28:12 +0200

    create first view

--- front-end/src/views/Landing.js
+++ front-end/src/views/Landing.js
@@ -4,18 +4,46 @@ export default class Landing extends Component {
   render() {
     return (
       <div>
-        <div className='corner-circles'>
-          <img src='/images/large-purple-ellipse.png' alt='corner purple ellipse'/>
-          <img src='/images/large-yellow-ellipse.png' alt='large yellow ellipse'/>
-          <img src='/images/small-purple-ellipse.png' alt='small purple ellipse'/>
-          <img src='/images/small-yellow-ellipse.png' alt='small yellow ellipse'/>
+        <div className="circles-container">
+          <img
+            className="big-purple"
+            src="/images/large-purple-ellipse.png"
+            alt="corner purple ellipse"
+          />
+          <img
+            className="big-yellow"
+            src="/images/large-yellow-ellipse.png"
+            alt="large yellow ellipse"
+          />
+          <img
+            className="small-purple"
+            src="/images/small-purple-ellipse.png"
+            alt="small purple ellipse"
+          />
+          <img
+            className="small-yellow"
+            src="/images/small-yellow-ellipse.png"
+            alt="small yellow ellipse"
+          />
         </div>
-        <div>
-          <h1>bibliofeels</h1>
-          <p>for the love of books.</p>
-          <a href='#'> {/**a place holder for the routing */}
-            <button>get started</button>
-          </a>
+        <div className="intro-container">
+          <div className="wrapper">
+            <header>
+              <div className="intro-content">
+                <h1>bibliofeels</h1>
+                <p className="p1">for the love of books.</p>
+                
+                  <a href="#" className='p2'>
+                    {" "}
+                    {/**a place holder for the routing */}
+                    <button className="w3-button w3-round-large w3-khaki">
+                      get started
+                    </button>
+                  </a>
+                
+              </div>
+            </header>
+          </div>
         </div>
       </div>
     );

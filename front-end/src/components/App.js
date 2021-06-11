171a1fac38a795dd62d7a3f89abe48db65787afe
Omar <omarmuhammed1998.om@gmail.com>
2021-06-11 22:46:40 +0200

    add the result view

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,22 +1,43 @@
 import React, { Component } from "react";
-import LandingView from '../views/LandingView'
-import InputView from '../views/InputView'
-import {Switch, Route, Redirect} from 'react-router-dom'
+import LandingView from "../views/LandingView";
+import InputView from "../views/InputView";
+import ResultView from "../views/ResultView";
+import { Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
-  
-  handleSubmitEmotion = (e, emotion) => {
-    e.preventDefault()
-    console.log(emotion)
+  constructor() {
+    super();
+    this.state = {
+      book: "",
+    };
   }
+
+  handleSubmitEmotion = async (e, emotion) => {
+    e.preventDefault();
+    const res = await fetch(`/recommend?emotions=${[emotion]}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const jsonResponse = await res.json();
+    console.log(jsonResponse[0].id)
+    this.setState({
+      book: jsonResponse[0]
+    })
+  };
   render() {
+    const {book} = this.state
     return (
       <div>
         <Switch>
-          <Route exact path='/' component={LandingView}/>
-          <Route exact path='/input' render={() => 
-            (<InputView onSubmit={this.handleSubmitEmotion}/>)
-          }/>
+          <Route exact path="/" component={LandingView} />
+          <Route
+            exact
+            path="/input"
+            render={() => <InputView onSubmit={this.handleSubmitEmotion} bookID={book.id}/>}
+          />
+          <Route exact path={`/book/:${book.id}`} render={() => <ResultView book={book}/>} />
         </Switch>
       </div>
     );
---
+++ front-end/src/views/ResultView.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+export default class ResultView extends Component {
+  render() {
+    const { book } = this.props;
+    return (
+      <div>
+        <div className="pg3-background">
+          <img
+            className="bg-pg3"
+            src="https://i.pinimg.com/originals/90/de/6a/90de6a722188fc9a9c0835db1f0e5500.jpg"
+            alt="background"
+          />
+          <img
+            className="book-img-holder"
+            src="https://media.istockphoto.com/photos/book-picture-id147705864?k=6&m=147705864&s=612x612&w=0&h=Pp0LSPgc4Z8y94pCke0zxLzJv7Sa9mw3WaHcIdExaPc="
+            alt="book cover"
+          />
+        </div>
+        <div className="new-background">
+          <p className="rec-book-title">Recommended book:</p>
+          <div className="keys">
+            <p className="title">Title:</p>
+            <p className="author">Author:</p>
+            {/* <p className="genre">Genre:</p> */}
+            {/* <p className="emotions">Emotions:</p> */}
+            <p className="wiki-link">Wiki link:</p>
+          </div>
+          <p className="summary-title">Summary:</p>
+          <p className="summary-text">{book.summary}</p>
+          <div className="values">
+            <p className="title">{book.title}</p>
+            <p className="author">{book.author}</p>
+            {/* <p className="genre">{JSON.parse(book.genre)[0]}</p> */}
+            {/* <p className="emotions">happy, nostalgic</p> */}
+            <a
+              href={`${book.wikipedia_link}`}
+              className="wiki-link"
+            >{`${book.wikipedia_link}`}</a>
+            
+          </div>
+          {/* <button className="button" type="submit">
+                suggest
+              </button> */}
+        </div>
+      </div>
+    );
+  }
+}
